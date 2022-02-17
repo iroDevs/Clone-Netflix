2d60416fc6985d1973842f470d8207840de7ec3a
iroDevs <ironegrolol@gamil.com>
2022-02-17 16:44:16 -0300

    componente em destaque

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import RequestApi from './RequestApi';
 import {React , useEffect , useState} from 'react';
 import Row from './components/Row'
+import FeatureMovie from './components/FeatureMovie';
 import './App.css';
 
 
 function App() {
    
   const [list , setList] = useState([])
+  const [featureData, setFeatureData] = useState(null)
 
   async function getAllMovies(){
     let Movielist = await RequestApi.getHomeList()
     setList(Movielist);
+
+    //vamos pegar um pra colocar em destaque feature Data
     return 0;
   }
 
@@ -22,6 +26,15 @@ function App() {
 
   return (
     <div >
+
+      {
+        featureData ? 
+        <FeatureMovie item={featureData} />
+        :
+        undefined
+      }
+
+      
       <section className="lists">
           {
             list.map((item,index) => {
---
+++ src/components/FeatureMovie.js
@@ -0,0 +1,11 @@
+
+import {React} from 'react'
+import './FeatureMovie.css'
+
+export default function FeatureMovie() {
+  return (
+   <div>
+     Feet
+   </div>
+  )
+}
