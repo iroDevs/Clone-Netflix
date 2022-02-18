ba7d94ee5b009defb61cbff087e129df6891f4aa
iroDevs <ironegrolol@gamil.com>
2022-02-18 00:18:38 -0300

    coloca no logo meu link pro github

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header({black}) {
   return (
     <header className={black ? 'black' : ''}>
       <div className="heaer-logo">
-        <a href="https://github.com/iroDevs/Clone-Netflix/tree/main/src" target='_blank' className='logo'>
+        <a href="https://github.com/iroDevs/Clone-Netflix/tree/main/src" target='_blank' className='logo' rel="noreferrer">
               PedroFlix
         </a>
       </div>
