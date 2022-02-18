4ab54ad4b208f2926bc6d36f54b57ab0a2d0437d
iroDevs <ironegrolol@gamil.com>
2022-02-18 00:57:48 -0300

    botoes criados

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,23 @@
-import React from 'react'
 import './Row.css'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+
+import React from 'react'
+
 export default function Row({title,items}) {
   const QtnFilms = items.results.length > 0
   const configUrl = 'https://image.tmdb.org/t/p/w300'
   return (
   <div className='Row'>
     <h2>{title}</h2>
+    <div className='movieRow-left'>
+    
+    <NavigateBeforeIcon style={{fontSize: 50}} />
+    </div>
+
+    <div className='movieRow-right'>
+    <NavigateNextIcon style={{fontSize: 50}} />
+    </div>
     <div className="row-listarea">
       <div className="movieRow-list">
     {QtnFilms ? 
